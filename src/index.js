716fc41f0926bf16f4ec18fb8c73c1b9326e6ac0
RebeccaRosies <rebecca.rosies@student.ehb.be>
2021-12-14 17:17:54 +0100

    request font over https (ipv http)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ const apiKey = c.apiKey
 
 function getSongs(){
     fetch(`https://api.musixmatch.com/ws/1.1/music.genres.get?apikey=${apiKey}`)
-    // fetch(`http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=vengabus&apikey=${apiKey}`)
-    // fetch(`http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=15953433&apikey=${apiKey}`)
+    // fetch(`https://api.musixmatch.com/ws/1.1/track.search?q_lyrics=vengabus&apikey=${apiKey}`)
+    // fetch(`https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=15953433&apikey=${apiKey}`)
     .then(response => response.json())
     .then(data =>
         console.log(data))
