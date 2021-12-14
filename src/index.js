3df0fd358fbe86c04d60ed4d5f1b01cd293e4c94
RebeccaRosies <rebecca.rosies@student.ehb.be>
2021-12-14 16:57:33 +0100

    fetch calls with https (ipv http)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const apiKey = ""
 
 function getSongs(){
-    fetch(`http://api.musixmatch.com/ws/1.1/music.genres.get?apikey=${apiKey}`)
+    fetch(`https://api.musixmatch.com/ws/1.1/music.genres.get?apikey=${apiKey}`)
     // fetch(`http://api.musixmatch.com/ws/1.1/track.search?q_lyrics=vengabus&apikey=${apiKey}`)
     // fetch(`http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=15953433&apikey=${apiKey}`)
     .then(response => response.json())
